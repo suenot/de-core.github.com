579bccdc6852cfcc309f4ecfbe4800f09b8a5d83
Eugen Soloviov <suenot@gmail.com>
2014-08-02 15:26:30 +0400

    fresh

---
+++ js/base.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+	//select2
+	// $('select.select2').select2();
+	$('#logo').load('img/de-core.svg');
+	$('#hide-if-svg').hide();
+	// footable
+	$('.footable').footable();
+
+	$('#filter').on('propertychange change keyup paste input', function(e) {
+		e.preventDefault();
+		$('.footable').trigger('footable_filter', {filter: $('#filter').val()});
+	});
+});
